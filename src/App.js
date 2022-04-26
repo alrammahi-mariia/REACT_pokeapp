753bf4465ab6e7b929b45519433b44837deb728d
Your Name <you@example.com>
2022-04-26 10:49:38 +0300

    routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from "react";
 import Layout from "./pages/Layout";
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./components/About";
+import FavList from "./components/FavList";
+import Home from "./components/Home";
+import PokeList from "./components/PokeList";
+import PokeSingle from "./components/PokeSingle";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <Layout />
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="pokelist" element={<PokeList />}>
+              <Route path="pokesingle" element={<PokeSingle />} />
+            </Route>
+            <Route path="/favlist" element={<FavList />} />
+            <Route path="/about" element={<About />} />
+          </Route>
+        </Routes>
       </BrowserRouter>
     </div>
   );
